d0a19d1c4cd5860d2cdaeb9f375a6757511fe95f
SMITHABHASKAR <smbh4256@colorado.edu>
2019-10-04 19:32:53 -0600

    Updated node js file

--- Project 2/serverPi/demo2.js
+++ Project 2/serverPi/demo2.js
@@ -2,15 +2,15 @@ const http = require('http');
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "EID",
-  password: "EID",
-  database: "TEMP",
+  host: 'localhost',
+  user: 'EID',
+  password: 'EID',
+  database: 'TEMP',
   charset: 'utf8'
 });
 
 //html string that will be send to browser
-var reo ='<html><head><title>Node.js MySQL Select</title></head><body><h1>Node.js MySQL Select</h1>{${table}}</body></html>';
+var reo ='<html><head><title>Temperature and humidity Readings</title></head><body><h1>Node.js MySQL Select</h1>{${table}}</body></html>';
 
 //sets and returns html table with results from sql select
 //Receives sql query and callback function to return the table
@@ -25,9 +25,9 @@ function setResHtml(sql, cb){
 
       //create html table with data from res.
       for(var i=0; i<res.length; i++){
-        table +='<tr><td>'+ (i+1) +'</td><td>'+ res[i].dateandtime +'</td><td>'+ res[i].TEMPERATUREinC + '</td></tr>';
+        table +='<tr><td>'+ res[i].dateandtime +'</td><td>'+ res[i].TEMPERATUREinC +'</td><td>'+ res[i].HUMIDITY +'</td></tr>';
       }
-      table ='<table border="1"><tr><th>Nr.</th><th>Name</th><th>Address</th></tr>'+ table +'</table>';
+      table ='<table border="1"><tr><th>DateandTime</th><th>TemperatureinC</th><th>Humidity</th></tr>'+ table +'</table>';
 
       con.release(); //Done with mysql connection
 
@@ -36,7 +36,7 @@ function setResHtml(sql, cb){
   });
 }
 
-let sql ='SELECT name, address FROM friends WHERE id >1 ORDER BY name';
+let sql ='SELECT * from Sensor LIMIT 10';
 
 //create the server for browser access
 const server = http.createServer((req, res)=>{
