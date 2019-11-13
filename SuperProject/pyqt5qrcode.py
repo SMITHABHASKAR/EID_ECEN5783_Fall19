a600dfb362461efde135e8c47d5e63591a797df1
Shanel Wu <shanel.wu@colorado.edu>
2019-11-13 16:49:15 -0700

    Adding pyqt code for superproject

---
+++ SuperProject/pyqt5qrcode.py
@@ -0,0 +1,98 @@
+#Citation: https://www.codenov.com/1540126802d2d175d9162e3bc0fff233
+
+
+import pyqrcode
+from pyzbar.pyzbar import decode, ZBarSymbol
+from PIL import Image
+import sys
+import os
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.uic import *
+from PyQt5 import QtCore,QtWidgets
+
+ 
+class qrForm(QMainWindow):
+    def __init__(self):
+        super().__init__()
+ 
+        self.setWindowTitle("QrGenerator")
+ 
+        self.setMinimumSize(QtCore.QSize(600,800))
+        self.setMaximumSize(QtCore.QSize(1500,900))
+        self.resize(600,800)
+ 
+        self.centralwidget = QWidget(self)
+        
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+ 
+        self.verticalLayout = QVBoxLayout()
+ 
+        self.qrTextEdit = QLineEdit(self.centralwidget)
+        self.verticalLayout.addWidget(self.qrTextEdit)
+ 
+        self.genQrButton = QPushButton(self.centralwidget)
+        self.genQrButton.setText("Generate QRCode")
+        self.verticalLayout.addWidget(self.genQrButton)
+ 
+        self.decodeQrButton = QPushButton(self.centralwidget)
+        self.decodeQrButton.setText("Read QRCode")
+        self.verticalLayout.addWidget(self.decodeQrButton)
+ 
+        self.decodeText = QLabel(self.centralwidget)
+        self.decodeText.setText("..")
+        self.decodeText.setAlignment(QtCore.Qt.AlignCenter)
+        self.verticalLayout.addWidget(self.decodeText)
+ 
+        self.qrCodeImage = QLabel(self.centralwidget)
+ 
+        self.sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        self.sizePolicy.setHorizontalStretch(1)
+        self.sizePolicy.setVerticalStretch(1)
+        self.sizePolicy.setHeightForWidth(self.qrCodeImage.sizePolicy().hasHeightForWidth())
+ 
+        self.qrCodeImage.setSizePolicy(self.sizePolicy)
+        self.qrCodeImage.setText("")
+        self.qrCodeImage.setAlignment(QtCore.Qt.AlignCenter)
+ 
+        self.verticalLayout.addWidget(self.qrCodeImage)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+ 
+        self.setCentralWidget(self.centralwidget)
+ 
+        self.genQrButton.clicked.connect(self.genButton)
+        self.decodeQrButton.clicked.connect(self.decQrButton)
+ 
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Open QRCode Image", "","Images (*.png);;All Files (*)", options=options)
+        if fileName:
+            return fileName
+        else:
+            return None
+ 
+ 
+    def genButton(self):
+        if(len(self.qrTextEdit.text()) < 1):
+            return
+        rQR = self.generateQR(self.qrTextEdit.text())
+        self.qrCodeImage.setPixmap(QPixmap("test.png").scaled(429,429))
+ 
+    def decQrButton(self):
+        fName = self.openFileNameDialog()
+        if (fName == None):
+            return
+        qr = decode(Image.open(fName), symbols=[ZBarSymbol.QRCODE])
+        extractString = str(qr[0].data)
+        self.decodeText.setText(extractString[2:-1])
+ 
+    def generateQR(self,str):
+        q = pyqrcode.create(str)
+        q.png("test.png",scale=13)
+ 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    pForm = qrForm()
+    pForm.show()
+    sys.exit(app.exec_())
---
+++ SuperProject/pyqtloom.py
@@ -0,0 +1,52 @@
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel
+from PyQt5.QtWidgets import QMainWindow,QApplication,QWidget,QPushButton,QAction,QLineEdit,QMessageBox
+from PyQt5.QtWidgets import QInputDialog, QLineEdit
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QIcon,QPixmap
+import sys
+
+
+class App(QWidget):
+       def __init__(self):
+               super().__init__()
+               self.title='Loom UI'
+               self.left=10
+               self.top=10
+               self.width=640
+               self.height=480
+               self.initUI()
+               QLabel().setText("welcome to Loom GUI Configuration")
+       def initUI(self):
+               self.setWindowTitle(self.title)
+               label=QLabel(self)
+               self.setGeometry(10,10,250,250)
+               pixmap=QPixmap('logo.png')
+               label.setPixmap(pixmap)
+               self.resize(pixmap.width(),pixmap.height())
+               self.setGeometry(self.left,self.top,self.width,self.height)
+               self.button=QPushButton('New Project',self)
+               self.button.clicked.connect(self.getText)
+               self.button.move(100,200)
+               
+               #self.getText()
+               button=QPushButton('Scan QR Code',self)               
+               button.move(100,250)
+               
+               buttonexit=QPushButton('Close GUI',self)
+               buttonexit.move(100,300)
+               buttonexit.clicked.connect(self.buttonexit)
+               self.show()
+       @pyqtSlot()
+       def getText(self):
+           
+           text, okPressed = QInputDialog.getText(self, "New Project Name","Your Project name:", QLineEdit.Normal, "")
+           if okPressed and text != '':
+               print(text)
+               
+       def buttonexit(self):
+           
+           QtCore.QCoreApplication.instance().quit()
+           
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    ex=App()
