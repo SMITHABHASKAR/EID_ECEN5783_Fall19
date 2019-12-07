a312976232652a3f0cc0986bf8b1e9df414b1aab
Shanel Wu <shanel.wu@colorado.edu>
2019-12-06 17:40:48 -0700

    fixed pedalGPIO.py

--- SuperProject/pedalGPIO.py
+++ SuperProject/pedalGPIO.py
@@ -1,4 +1,5 @@
 from gpiozero import *
+from signal import pause
 from PyQt5 import QtCore, QtGui, QtWidgets, QtNetwork
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -10,7 +11,9 @@ class pedalHandler(QObject):
     reversePedalEvent = pyqtSignal()
     loomRelayEvent = pyqtSignal()
     
-    def __init__(self, loom):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
         # GPIO pins on Pi, all clustered on corner close to USB ports
         self.advancePedal = Button(16) # set to pull-up by default
         self.refreshPedal = Button(20)
@@ -19,27 +22,30 @@ class pedalHandler(QObject):
 
         self.pedals = [self.advancePedal, self.refreshPedal, self.reversePedal]
 
-        self.advancePedal.when_pressed = advancePressed
-        self.advancePedal.when_released = advancePressed
-        self.refreshPedal.when_pressed = refreshPressed
-        self.refreshPedal.when_released = refreshPressed
-        self.reversePedal.when_pressed = reversePressed
-        self.reversePedal.when_released = reversePressed
+        self.advancePedal.when_pressed = self.advancePressed
+        self.advancePedal.when_released = self.advancePressed
+        self.refreshPedal.when_pressed = self.refreshPressed
+        self.refreshPedal.when_released = self.refreshPressed
+        self.reversePedal.when_pressed = self.reversePressed
+        self.reversePedal.when_released = self.reversePressed
 
-    def advancePressed():
+    def advancePressed(self):
         print ("advance pedal pressed")
-        advancePedalEvent.emit()
-    def refreshPressed():
+        self.advancePedalEvent.emit()
+        self.sendToRelay()
+    def refreshPressed(self):
         print ("refresh pedal pressed")
-        refreshPedalEvent.emit()
-    def reversePressed():
+        self.refreshPedalEvent.emit()
+        self.sendToRelay()
+    def reversePressed(self):
         print ("reverse pedal pressed")
-        reversePedalEvent.emit()
+        self.reversePedalEvent.emit()
+        self.sendToRelay()
 
-    def sendToRelay():
+    def sendToRelay(self):
         print ("sending message to relay")
-        self.loomRelay.blink(0.5, 0.5, 1) # turn on/off once to simulate pulse
-        loomRelayEvent.emit()
+        self.loomRelay.blink(0.3, 0.3, 1) # turn on/off once to simulate pedal step on/off
+        self.loomRelayEvent.emit()
 
 if __name__ == "__main__":
     p = pedalHandler()
